277baef99c3dde46375423b591d1a40607bdfe97
Tse Kit Yam <me@kytse.com>
2017-05-16 22:15:43 +0800

    Update initialization

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -30,10 +30,10 @@ UKF::UKF() {
         0, 0, 0, 0, 1;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 1;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 1;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
