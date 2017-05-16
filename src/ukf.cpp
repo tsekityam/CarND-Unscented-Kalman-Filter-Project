85786dadd6b31e505211ba2603e7b6152003a479
Tse Kit Yam <me@kytse.com>
2017-05-16 22:16:41 +0800

    Remove TODOs

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -50,12 +50,6 @@ UKF::UKF() {
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
 
-  /**
-  TODO:
-
-  Hint: one or more values initialized above might be wildly off...
-  */
-
   //set state dimension
   n_x_ = 5;
 
@@ -83,12 +77,6 @@ UKF::~UKF() {}
  * either radar or laser.
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-  TODO:
-
-  Complete this function! Make sure you switch between lidar and radar
-  measurements.
-  */
 
   if (is_initialized_) {
     double delta_t = (meas_package.timestamp_ - time_us_) / 1000000.0;
@@ -133,12 +121,6 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  * measurement and this one.
  */
 void UKF::Prediction(double delta_t) {
-  /**
-  TODO:
-
-  Complete this function! Estimate the object's location. Modify the state
-  vector, x_. Predict sigma points, the state, and the state covariance matrix.
-  */
   //create augmented mean vector
   VectorXd x_aug = VectorXd(n_aug_);
 
@@ -242,14 +224,6 @@ void UKF::Prediction(double delta_t) {
  * @param {MeasurementPackage} meas_package
  */
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
-  /**
-  TODO:
-
-  Complete this function! Use lidar data to update the belief about the object's
-  position. Modify the state vector, x_, and covariance, P_.
-
-  You'll also need to calculate the lidar NIS.
-  */
   int n_z = 2;
 
   //create matrix for sigma points in measurement space
@@ -337,14 +311,6 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
  * @param {MeasurementPackage} meas_package
  */
 void UKF::UpdateRadar(MeasurementPackage meas_package) {
-  /**
-  TODO:
-
-  Complete this function! Use radar data to update the belief about the object's
-  position. Modify the state vector, x_, and covariance, P_.
-
-  You'll also need to calculate the radar NIS.
-  */
 
   //set measurement dimension, radar can measure r, phi, and r_dot
   int n_z = 3;
